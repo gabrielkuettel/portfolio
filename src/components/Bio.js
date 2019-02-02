e2ffbe31b09aa2778fc632c21568c25d6195926e
Gabriel Kuettel <gabrielkuettel@gmail.com>
2019-02-01 18:01:03 -0700

    prettify syntax

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,67 +1,68 @@
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';
 import { FaMountain } from 'react-icons/fa';
-import { rhythm } from "../utils/typography"
+import { rhythm } from '../utils/typography';
 
 function Bio() {
-  return (
-    <StaticQuery
-      query={bioQuery}
-      render={data => {
-        const { author, social } = data.site.siteMetadata
-        return (
-          <div
-            style={{
-              display: `flex`,
-              marginBottom: rhythm(2.5),
-            }}
-          >
-            <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              style={{
-                marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `100%`,
-              }}
-              imgStyle={{
-                borderRadius: `50%`,
-              }}
-            />
-            <p>
-              Portfolio for <strong>{author}</strong> who lives and works in Boulder, Colorado. <FaMountain />
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                Follow him on Twitter.
-              </a>
-            </p>
-          </div>
-        )
-      }}
-    />
-  )
+   return (
+      <StaticQuery
+         query={bioQuery}
+         render={data => {
+            const { author, social } = data.site.siteMetadata;
+            return (
+               <div
+                  style={{
+                     display: `flex`,
+                     marginBottom: rhythm(2.5)
+                  }}
+               >
+                  <Image
+                     fixed={data.avatar.childImageSharp.fixed}
+                     alt={author}
+                     style={{
+                        marginRight: rhythm(1 / 2),
+                        marginBottom: 0,
+                        minWidth: 50,
+                        borderRadius: `100%`
+                     }}
+                     imgStyle={{
+                        borderRadius: `50%`
+                     }}
+                  />
+                  <p>
+                     Portfolio for <strong>{author}</strong> who lives and works
+                     in Boulder, Colorado. <FaMountain />
+                     {` `}
+                     <a href={`https://twitter.com/${social.twitter}`}>
+                        Follow him on Twitter.
+                     </a>
+                  </p>
+               </div>
+            );
+         }}
+      />
+   );
 }
 
 const bioQuery = graphql`
-  query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-      childImageSharp {
-        fixed(width: 50, height: 50) {
-          ...GatsbyImageSharpFixed
-        }
+   query BioQuery {
+      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+         childImageSharp {
+            fixed(width: 50, height: 50) {
+               ...GatsbyImageSharpFixed
+            }
+         }
       }
-    }
-    site {
-      siteMetadata {
-        author
-        social {
-          twitter
-        }
+      site {
+         siteMetadata {
+            author
+            social {
+               twitter
+            }
+         }
       }
-    }
-  }
-`
+   }
+`;
 
-export default Bio
+export default Bio;
