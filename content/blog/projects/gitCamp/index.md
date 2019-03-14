f7e1b1f1f2fa039c985f11301636ac7e6853fff5
Gabriel Kuettel <gabrielkuettel@gmail.com>
2019-03-14 17:48:28 -0600

    add content

---
+++ content/blog/projects/gitCamp/index.md
@@ -0,0 +1,16 @@
+---
+title: "gitCamp"
+subtitle: "fullstack app"
+order: 1
+date: "2014-05-06T23:46:37.121Z"
+img: "gitcamp-logo.png"
+---
+
+Wow! I love blogging so much already.
+
+Did you know that "despite its name, salted duck eggs can also be made from
+chicken eggs, though the taste and texture will be somewhat different, and the
+egg yolk will be less rich."?
+([Wikipedia Link](http://en.wikipedia.org/wiki/Salted_duck_egg))
+
+Yeah, I didn't either.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,116 +1,115 @@
-import React from 'react';
-import { Link, graphql } from 'gatsby';
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import Layout from '../components/Layout';
-import SEO from '../components/seo';
-import { rhythm } from '../utils/typography';
-import styles from './index.module.css';
-import WebImage from '../components/Image/WebImage';
-import Image from '../components/Image/Image';
+import Layout from "../components/Layout";
+import SEO from "../components/seo";
+import { rhythm } from "../utils/typography";
+import styles from "./index.module.css";
+import WebImage from "../components/Image/WebImage";
+import Image from "../components/Image/Image";
 
 class BlogIndex extends React.Component {
-   render() {
-      const { data } = this.props;
-      const siteTitle = data.site.siteMetadata.title;
-      const posts = data.allMarkdownRemark.edges;
+  render() {
+    const { data } = this.props;
+    const siteTitle = data.site.siteMetadata.title;
+    const posts = data.allMarkdownRemark.edges;
 
-      return (
-         <Layout location={this.props.location} title={siteTitle}>
-            <SEO
-               title="All posts"
-               keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-            />
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="All posts"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
 
-            <div className={styles.gridWrapper}>
-               {posts.map(({ node }) => {
-                  const slug = node.fields.slug.slice(1);
-                  const title = node.frontmatter.title || node.fields.slug;
-                  const subtitle = node.frontmatter.subtitle || null;
+        <div className={styles.gridWrapper}>
+          {posts.map(({ node }) => {
+            const slug = node.fields.slug.slice(1);
+            const title = node.frontmatter.title || node.fields.slug;
+            const subtitle = node.frontmatter.subtitle || null;
 
-                  let image = 'placeholder.jpg';
+            let image = "placeholder.jpg";
 
-                  if (node.frontmatter.img) {
-                     image = node.frontmatter.img.relativePath;
-                  }
+            if (node.frontmatter.img) {
+              image = node.frontmatter.img.relativePath;
+            }
 
-                  console.log('image', image);
+            console.log("image", image);
 
-                  return (
-                     <div
-                        key={node.fields.slug}
-                        className={
-                           node.frontmatter.feature
-                              ? styles.feature
-                              : styles.panel
-                        }
-                     >
-                        <div className={styles.container}>
-                           <Link to={node.fields.slug}>
-                              <h3 className={styles.title}>{title}</h3>
-                              <Image src={image} />
-                              <h5 className={styles.subtitle}>
-                                 {subtitle ? <>{subtitle}</> : null}
-                              </h5>
-                           </Link>
-                        </div>
-                     </div>
-                     // <div key={node.fields.slug}>
-                     //    <p>
+            return (
+              <div
+                key={node.fields.slug}
+                className={
+                  node.frontmatter.feature ? styles.feature : styles.panel
+                }
+              >
+                <div className={styles.container}>
+                  <Link to={node.fields.slug}>
+                    <h3 className={styles.title}>{title}</h3>
+                    <Image src={image} />
+                    <h5 className={styles.subtitle}>
+                      {subtitle ? <>{subtitle}</> : null}
+                    </h5>
+                  </Link>
+                </div>
+              </div>
+              // <div key={node.fields.slug}>
+              //    <p>
 
-                     //    </p>
-                     //    <h3
-                     //       style={{
-                     //          marginBottom: rhythm(1 / 4)
-                     //       }}
-                     //    >
-                     //       <Link
-                     //          style={{ boxShadow: `none` }}
-                     //          to={node.fields.slug}
-                     //       >
-                     //          {title}
-                     //       </Link>
-                     //    </h3>
-                     //    <small>{node.frontmatter.date}</small>
-                     //    <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-                     // </div>
-                  );
-               })}
+              //    </p>
+              //    <h3
+              //       style={{
+              //          marginBottom: rhythm(1 / 4)
+              //       }}
+              //    >
+              //       <Link
+              //          style={{ boxShadow: `none` }}
+              //          to={node.fields.slug}
+              //       >
+              //          {title}
+              //       </Link>
+              //    </h3>
+              //    <small>{node.frontmatter.date}</small>
+              //    <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+              // </div>
+            );
+          })}
 
-               <div className={styles.footer} />
-            </div>
-         </Layout>
-      );
-   }
+          <div className={styles.footer} />
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default BlogIndex;
 
 export const pageQuery = graphql`
-   query {
-      site {
-         siteMetadata {
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___order], order: ASC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
             title
-         }
-      }
-      allMarkdownRemark(sort: { fields: [frontmatter___title], order: ASC }) {
-         edges {
-            node {
-               excerpt
-               fields {
-                  slug
-               }
-               frontmatter {
-                  date(formatString: "MMMM DD, YYYY")
-                  title
-                  feature
-                  subtitle
-                  img {
-                     id
-                     relativePath
-                  }
-               }
+            feature
+            subtitle
+            order
+            img {
+              id
+              relativePath
             }
-         }
+          }
+        }
       }
-   }
+    }
+  }
 `;
