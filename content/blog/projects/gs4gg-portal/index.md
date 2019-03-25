d465b0029cc450aa6f83c1755ca6fa970945d2ab
Gabriel Kuettel <gabrielkuettel@gmail.com>
2019-03-24 18:56:44 -0600

    add latest

--- content/blog/projects/gs4gg-portal/index.md
+++ content/blog/projects/gs4gg-portal/index.md
@@ -1,12 +1,12 @@
 ---
 title: "Project Developer Portal"
-subtitle: "Wordpress"
-tools: "Wordpress/PHP, Advanced Custom Fields, Adobe Muse"
-date: "2015-05-01T22:12:03.284Z"
+subtitle: "Fullstack Development"
+tools: "Wordpress/PHP, ACF, Adobe Muse"
+date: "2017"
 img: "thumbnail.jpg"
 order: 8
 ---
 
-<video style="padding: 0 0 40px 0;" width="100%" autoplay controls loop>
+<video style="padding: 0 0 40px 0;" width="100%" muted controls loop>
     <source src="gs4gg-portal-demo.mp4" type="video/mp4">
 </video>
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -13,53 +13,51 @@ class BlogPostTemplate extends Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title={post.frontmatter.title} description={post.excerpt} />
-
-        <h1>{post.frontmatter.title}</h1>
-        <p
+        <div
           style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1)
+            background: `white`,
+            padding: `${rhythm(1 / 2)} ${rhythm(1)}`
           }}
         >
-          {post.frontmatter.subtitle} | {post.frontmatter.tools} |{" "}
-          {post.frontmatter.date}
-        </p>
-
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-
-        <hr
-          style={{
-            marginBottom: rhythm(1)
-          }}
-        />
-
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-            margin: 0
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
+          <h1>{post.frontmatter.title}</h1>
+          <p
+            style={{
+              ...scale(-1 / 5),
+              display: `block`,
+              marginBottom: rhythm(1),
+              marginTop: rhythm(-1)
+            }}
+          >
+            {post.frontmatter.subtitle} | {post.frontmatter.tools} |{" "}
+            {post.frontmatter.date}
+          </p>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          <ul
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+              margin: 0
+            }}
+          >
+            <li>
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </div>
       </Layout>
     );
   }
