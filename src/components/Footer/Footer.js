2f3ed870b3f308f4683d9da6e0bbe1c9dd1c7c95
Gabriel Kuettel <gabrielkuettel@gmail.com>
2019-03-24 20:07:01 -0600

    add latest

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,15 @@ import styles from "./footer.module.css";
 const Footer = props => (
   <footer className={styles.footer}>
     Designed and built by {` `}
-    <a href="mailto:gabrielkuettel@gmail.com">Gabriel Kuettel</a> (c){" "}
-    {new Date().getFullYear()}
+    <a href="mailto:gabrielkuettel@gmail.com">Gabriel Kuettel</a> | Copyright
+    &copy; {new Date().getFullYear()} |{" "}
+    <a
+      href="https://github.com/gabrielkuettel/portfolio"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      View Source
+    </a>
   </footer>
 );
 
