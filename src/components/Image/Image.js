e1240247aaea15d6b6865be9989eb3d8044c6c21
Gabriel Kuettel <gabrielkuettel@gmail.com>
2019-11-14 23:32:43 -0700

    change images to traced svgs

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,47 +3,49 @@ import Img from "gatsby-image";
 import { StaticQuery, graphql } from "gatsby";
 
 const renderImage = file => {
-  return (
-    <Img
-      alt={`gabriel kuettel`}
-      title={`gabriel kuettel`}
-      fluid={file.node.childImageSharp.fluid}
-      style={{
-        height: `100%`,
-        width: `100%`,
-        objectFit: `cover`
-      }}
-    />
-  );
+	return (
+		<Img
+			alt={`gabriel kuettel`}
+			title={`gabriel kuettel`}
+			fluid={file.node.childImageSharp.fluid}
+			style={{
+				height: `100%`,
+				width: `100%`,
+				objectFit: `cover`
+			}}
+		/>
+	);
 };
 
 const Image = ({ src }) => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query {
-          images: allFile(
-            filter: { extension: { regex: "/jpeg|jpg|png|gif/" } }
-          ) {
-            edges {
-              node {
-                extension
-                relativePath
-                childImageSharp {
-                  fluid(maxWidth: 1000) {
-                    ...GatsbyImageSharpFluid
-                  }
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={({ images }) =>
-        renderImage(images.edges.find(image => image.node.relativePath === src))
-      }
-    />
-  );
+	return (
+		<StaticQuery
+			query={graphql`
+				query {
+					images: allFile(
+						filter: { extension: { regex: "/jpeg|jpg|png|gif/" } }
+					) {
+						edges {
+							node {
+								extension
+								relativePath
+								childImageSharp {
+									fluid(maxWidth: 1000) {
+										...GatsbyImageSharpFluid_tracedSVG
+									}
+								}
+							}
+						}
+					}
+				}
+			`}
+			render={({ images }) =>
+				renderImage(
+					images.edges.find(image => image.node.relativePath === src)
+				)
+			}
+		/>
+	);
 };
 
 export default Image;
