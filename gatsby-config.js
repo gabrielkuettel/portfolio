4e32699d4aff1e184de8e87bcea06acd8b33c0fc
Gabriel Kuettel <gabrielkuettel@gmail.com>
2019-11-18 16:04:55 -0700

    configure styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,7 @@ module.exports = {
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
 		`gatsby-plugin-feed`,
+		`gatsby-plugin-styled-components`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
