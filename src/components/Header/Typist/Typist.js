a3b1613ef63fcb7c813329e41bbd4f1bb57f1276
Gabriel Kuettel <gabrielkuettel@gmail.com>
2019-11-17 21:17:45 -0700

    update typist component

--- src/components/Header/Typist/Typist.js
+++ src/components/Header/Typist/Typist.js
@@ -2,36 +2,26 @@ import React from 'react';
 import Typing from 'react-typing-animation';
 
 const AnimatedTypingComponent = ({ pathname, phrases }) => {
-   return (
-      <Typing>
-         {pathname === `${__PATH_PREFIX__}/` ? (
-            <>
-               {phrases.map((phrase, index) => {
-                  if (index === 0) {
-                     return (
-                        <div key={phrase}>
-                           {phrase}
-                           <Typing.Delay ms={2000} />
-                           <Typing.Backspace count={phrase.length} />
-                        </div>
-                     );
-                  } else {
-                     return (
-                        <div key={phrase}>
-                           {phrase}
-                           <Typing.Delay ms={1000} />
-                           <Typing.Backspace count={phrase.length} />
-                        </div>
-                     );
-                  }
-               })}
-               How can I <a href="mailto:gkdesigndev@gmail.com">help?</a>
-            </>
-         ) : (
-            <>{pathname}</>
-         )}
-      </Typing>
-   );
+	return (
+		<Typing>
+			{pathname === `${__PATH_PREFIX__}/` ? (
+				<>
+					{phrases.map((phrase, index) => {
+						return (
+							<div key={phrase}>
+								{phrase}
+								<Typing.Delay ms={1000} />
+								<Typing.Backspace count={phrase.length} />
+							</div>
+						);
+					})}
+					How can I <a href="mailto:gkdesigndev@gmail.com">help?</a>
+				</>
+			) : (
+				<>{pathname}</>
+			)}
+		</Typing>
+	);
 };
 
 export default AnimatedTypingComponent;
